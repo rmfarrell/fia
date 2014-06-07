284a56e9b6769d9b6e5d02840f0b9c9e79dfdbd2
Ryan Farrell <ryan.farrell@mcgarrybowen.com>
2014-06-07 12:29:02 -0400

    bootsrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fia::Application.config.secret_key_base = '8c997f2f4e0341f423fb13ca1747464dea88e54d3f3d48517c6e54b7356ebd15ada2568dbf574bda5d469914d4e720848fa693e5c20305ac007b5c45b2d231ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fia::Application.config.session_store :cookie_store, key: '_fia_session'
