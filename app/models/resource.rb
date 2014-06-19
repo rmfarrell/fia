e3226eae24291d817754ded3a2f557609836cf1d
Ryan Farrell <ryan.farrell@mcgarrybowen.com>
2014-06-19 15:24:08 -0400

    trying lowerase column names

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,11 +2,11 @@ class Resource < ActiveRecord::Base
   
   scope :oidTest, -> (oid) { where oid: oid }
   scope :urlTest, -> (retrievalurl) { where retrievalURL: retrievalurl }
-  scope :objectTypeTest, -> (objecttype) {where('objectType=? OR objectType=?', objecttype, 'any')}
+  scope :objectTypeTest, -> (objecttype) {where('objecttype=? OR objecttype=?', objecttype, 'any')}
   scope :langTypeTest, -> (lang) { where('lang=? OR lang=?', 'any', lang) }
   #scope :search, -> (search) {where description: search}
   scope :searchDescription, -> (search) { where("description LIKE ?", "%#{search}%".downcase)}
-  scope :eventTypeTest, -> (eventtype) {where('eventType=? OR eventType=?', eventtype, 'any')}
+  scope :eventTypeTest, -> (eventtype) {where('eventtype=? OR eventtype=?', eventtype, 'any')}
   scope :isActive, -> () { where activeTo: '9999-12-31'}
   
   # scope :status, -> (status) { where status: status }
